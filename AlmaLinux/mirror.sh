6cd39152b1a835a4c7d276e33fdff489452848f4
Werner Strydom <bloudraak@users.noreply.github.com>
2022-06-28 10:55:37 -0700

    Adds AlmaLinux

---
+++ AlmaLinux/mirror.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+src="rsync://rsync.repo.almalinux.org/almalinux/"
+dest="/volume1/mirrors/pub/almalinux/"
+mkdir -p $dest
+
+rsync \
+    --archive \
+    --update \
+    --compress \
+    --hard-links \
+    --bwlimit=20m \
+    --delete \
+    --delete-after \
+    --delay-updates \
+    --timeout=600 \
+    --cvs-exclude \
+    --recursive \
+    --prune-empty-dirs \
+    --exclude-from="$SCRIPT_DIR/exclude.txt" \
+    --include-from="$SCRIPT_DIR/include.txt" \
+    $src \
+    $dest 
