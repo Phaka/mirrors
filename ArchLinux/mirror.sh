20e0140c130e082f8abc4f5235d7371a6bf5a349
Werner Strydom <bloudraak@users.noreply.github.com>
2022-06-29 11:07:35 -0700

    Adds ArchLinux

---
+++ ArchLinux/mirror.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+src="rsync://mirror.leaseweb.com/archlinux/"
+dest="/volume1/mirrors/pub/archlinux/"
+
+rsync \
+    --verbose \
+    --archive \
+    --update \
+    --compress \
+    --hard-links \
+    --bwlimit=20m \
+    --delete \
+    --delete-after \
+    --delay-updates \
+    --timeout=600 \
+    --recursive \
+    --include="*/" \
+    --include="*.iso" \
+    --include="*.iso.sig" \
+    --include="sha256sums.txt" \
+    --include="md5sums.txt" \
+    --exclude="*" \
+    --prune-empty-dirs \
+    $src \
+    $dest
