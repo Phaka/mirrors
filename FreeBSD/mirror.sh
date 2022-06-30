6975075d6103f26c099db9f712a97fa20ac661f4
Werner Strydom <bloudraak@users.noreply.github.com>
2022-06-30 08:12:09 -0700

    Adds Support for Legacy FreeBSD releases

--- FreeBSD/mirror.sh
+++ FreeBSD/mirror.sh
@@ -34,4 +34,65 @@ rsync \
     $src \
     $dest
 
+dest="/volume1/mirrors/pub/freebsd/FreeBSD-Archive/old-releases/"
+mkdir -p $dest
+src="rsync://ftp-archive.freebsd.org/FreeBSD-Archive/old-releases/"
+rsync \
+    -vv \
+    --list-only \
+    --archive \
+    --update \
+    --compress \
+    --bwlimit=20000 \
+    --delete \
+    --delete-after \
+    --delay-updates \
+    --timeout=600 \
+    --recursive \
+    --safe-links \
+    --fuzzy \
+    --exclude-from="$SCRIPT_DIR/../exclude-arch.txt" \
+    --exclude="*BETA*" \
+    --exclude="1.*" \
+    --exclude="2.*" \
+    --exclude="3.*" \
+    --exclude="4.*" \
+    --exclude="5.*" \
+    --exclude="6.*" \
+    --exclude="7.*" \
+    --exclude="8.*" \
+    --exclude="9.*" \
+    --exclude="FreeBSD-1.*" \
+    --exclude="FreeBSD-2.*" \
+    --exclude="FreeBSD-3.*" \
+    --exclude="FreeBSD-4.*" \
+    --exclude="FreeBSD-5.*" \
+    --exclude="FreeBSD-6.*" \
+    --exclude="FreeBSD-7.*" \
+    --exclude="FreeBSD-8.*" \
+    --exclude="FreeBSD-9.*" \
+    --exclude="10.0-RELEASE" \
+    --exclude="10.1-RELEASE" \
+    --exclude="10.2-RELEASE" \
+    --exclude="10.3-RELEASE" \
+    --exclude="11.0-RELEASE" \
+    --exclude="11.1-RELEASE" \
+    --exclude="12.0-RELEASE" \
+    --exclude="12.1-RELEASE" \
+    --exclude="12.2-RELEASE" \
+    --exclude="13.0-RELEASE" \
+    --exclude="README.TXT" \
+    --exclude="TIMESTAMP" \
+    --exclude="CI-IMAGES" \
+    --exclude="/ISO-IMAGES" \
+    --exclude="VM-IMAGES" \
+    --exclude="*-bootonly.iso" \
+    --exclude="*-disc1.iso" \
+    --include="*.iso" \
+    --include="CHECKSUM*" \
+    --include="SHA*" \
+    --include="*/" \
+    --exclude="*" \
+    --prune-empty-dirs \
+    $src \
 
