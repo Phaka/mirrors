6bf63f4de2df55709f9c2267b9cb3a4eb7fd1f5a
Werner Strydom <bloudraak@users.noreply.github.com>
2022-06-30 14:09:52 -0700

    Refactored FreeBSD

--- FreeBSD/mirror.sh
+++ FreeBSD/mirror.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 dest="/volume1/mirrors/pub/freebsd/releases/"
 mkdir -p $dest
-
-src="rsync://mirror.liquidtelecom.com/freebsd/releases/"
+src="rsync://ftp-archive.freebsd.org/FreeBSD/releases/"
 rsync \
-    --dry-run \
     --verbose \
+    --human-readable \
     --archive \
     --update \
     --compress \
@@ -19,14 +19,11 @@ rsync \
     --no-motd \
     --safe-links --copy-links --hard-links \
     --fuzzy \
-    --exclude-from="$SCRIPT_DIR/../exclude-arch.txt" \
-    --exclude="README.TXT" \
-    --exclude="TIMESTAMP" \
-    --exclude="CI-IMAGES" \
-    --exclude="/ISO-IMAGES" \
-    --exclude="VM-IMAGES" \
+    --exclude="*-RC*" \
     --exclude="*-bootonly.iso" \
     --exclude="*-disc1.iso" \
+    --exclude="VM-IMAGES/" \
+    --exclude-from="$SCRIPT_DIR/../exclude-arch2.txt" \
     --include="*.iso" \
     --include="CHECKSUM*" \
     --include="SHA*" \
@@ -36,11 +33,12 @@ rsync \
     $src \
     $dest
 
-dest="/volume1/mirrors/pub/freebsd/FreeBSD-Archive/old-releases/"
+dest="/volume1/mirrors/pub/freebsd/old-releases/ISO-IMAGES/"
 mkdir -p $dest
-src="rsync://ftp-archive.freebsd.org/FreeBSD-Archive/old-releases/"
+src="rsync://ftp-archive.freebsd.org/FreeBSD-Archive/old-releases/ISO-IMAGES/"
 rsync \
     --verbose \
+    --human-readable \
     --archive \
     --update \
     --compress \
@@ -52,43 +50,11 @@ rsync \
     --no-motd \
     --safe-links --copy-links --hard-links \
     --fuzzy \
-    --exclude-from="$SCRIPT_DIR/../exclude-arch.txt" \
-    --exclude="*BETA*" \
-    --exclude="1.*" \
-    --exclude="2.*" \
-    --exclude="3.*" \
-    --exclude="4.*" \
-    --exclude="5.*" \
-    --exclude="6.*" \
-    --exclude="7.*" \
-    --exclude="8.*" \
-    --exclude="9.*" \
-    --exclude="FreeBSD-1.*" \
-    --exclude="FreeBSD-2.*" \
-    --exclude="FreeBSD-3.*" \
-    --exclude="FreeBSD-4.*" \
-    --exclude="FreeBSD-5.*" \
-    --exclude="FreeBSD-6.*" \
-    --exclude="FreeBSD-7.*" \
-    --exclude="FreeBSD-8.*" \
-    --exclude="FreeBSD-9.*" \
-    --exclude="10.0-RELEASE" \
-    --exclude="10.1-RELEASE" \
-    --exclude="10.2-RELEASE" \
-    --exclude="10.3-RELEASE" \
-    --exclude="11.0-RELEASE" \
-    --exclude="11.1-RELEASE" \
-    --exclude="12.0-RELEASE" \
-    --exclude="12.1-RELEASE" \
-    --exclude="12.2-RELEASE" \
-    --exclude="13.0-RELEASE" \
-    --exclude="README.TXT" \
-    --exclude="TIMESTAMP" \
-    --exclude="CI-IMAGES" \
-    --exclude="/ISO-IMAGES" \
-    --exclude="VM-IMAGES" \
+    --exclude="*-RC*" \
     --exclude="*-bootonly.iso" \
     --exclude="*-disc1.iso" \
+    --exclude="*-bootonly.iso" \
+    --exclude-from="$SCRIPT_DIR/../exclude-arch2.txt" \
     --include="*.iso" \
     --include="CHECKSUM*" \
     --include="SHA*" \
@@ -96,4 +62,4 @@ rsync \
     --exclude="*" \
     --prune-empty-dirs \
     $src \
-
+    $dest
