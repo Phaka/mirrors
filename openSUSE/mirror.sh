4e0e4971041dc594cd19e5677159b234a0082d60
Werner Strydom <bloudraak@users.noreply.github.com>
2022-06-30 15:33:28 -0700

    Housekeeping

--- openSUSE/mirror.sh
+++ openSUSE/mirror.sh
@@ -1,85 +1,29 @@
 #!/bin/bash
 set -euxo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-src="rsync://mirror.leaseweb.com/opensuse/tumbleweed/"
-dest="/volume1/mirrors/pub/opensuse/tumbleweed/"
-mkdir -p $dest
+src="rsync://mirror.leaseweb.com/opensuse/"
+dest="/volume1/mirrors/pub/opensuse/"
 rsync \
-    --dry-run \
     --verbose \
+    --human-readable \
     --archive \
     --update \
     --compress \
     --bwlimit=20000 \
-    --delete \
-    --delete-after \
     --delay-updates \
     --timeout=600 \
     --no-motd \
-    --cvs-exclude \
     --safe-links --copy-links --hard-links \
     --fuzzy \
-    --exclude-from="$SCRIPT_DIR/../exclude-arch.txt" \
-    --exclude="repo" \
-    --exclude="appliances" \
-    --exclude="iso-devel" \
-    --exclude="jeos" \
-    --exclude="live" \
-    --exclude="microos" \
-    --exclude="leap-micro" \
-    --exclude="*NET*" \
-    --exclude="*aarch64*" \
-    --exclude="*ppc64le*" \
-    --exclude="*s390x*" \
-    --exclude="*GNOME*" \
-    --exclude="*KDE*" \
-    --exclude="*Rescue*" \
-    --exclude="*XFCE*" \
-    --exclude="*MicroOS*" \
-    --exclude="*Kubic*" \
-    --include="*/" \
-    --include="*.iso" \
-    --include="*.iso.sha256" \
-    --include="*.iso.sha256.asc" \
-    --exclude="*" \
-    --prune-empty-dirs \
-    $src \
-    $dest
-
-src="rsync://mirror.leaseweb.com/opensuse/distribution/leap/"
-dest="/volume1/mirrors/pub/opensuse/distribution/leap/"
-mkdir -p $dest
-rsync \
-    --verbose \
-    --archive \
-    --update \
-    --compress \
-    --bwlimit=20000 \
-    --delete \
-    --delete-after \
-    --delay-updates \
-    --timeout=600 \
-    --no-motd \
-    --cvs-exclude \
-    --exclude="repo" \
-    --exclude="appliances" \
-    --exclude="iso-devel" \
-    --exclude="jeos" \
-    --exclude="live" \
-    --exclude="microos" \
-    --exclude="leap-micro" \
-    --exclude="*NET*" \
-    --exclude="*aarch64*" \
-    --exclude="*ppc64le*" \
-    --exclude="*s390x*" \
-    --exclude="*Snapshot*" \
-    --exclude="*GNOME*" \
-    --exclude="*KDE*" \
-    --exclude="*Rescue*" \
-    --exclude="*XFCE*" \
-    --exclude="*Current.iso" \
-    --exclude="*Current.iso.sha256" \
-    --exclude="*Current.iso.sha256.asc" \
+    --exclude="iso-devel/"  \
+    --exclude="Changes/" \
+    --exclude="appliances/" \
+    --exclude="*-Rescue-*" \
+    --exclude="*-NET-*" \
+    --exclude="*-MicroOS-*" \
+    --exclude="*-Kubic-*" \
+    --exclude-from="$SCRIPT_DIR/../exclude-arch2.txt" \
+    --exclude-from="$SCRIPT_DIR/../exclude-desktops.txt" \
     --include="*/" \
     --include="*.iso" \
     --include="*.iso.sha256" \
