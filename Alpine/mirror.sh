6c5754fceb59540512d051986039faa5189fa1f3
Werner Strydom <bloudraak@users.noreply.github.com>
2022-06-30 10:47:36 -0700

    Refactored Alpine Linux

--- Alpine/mirror.sh
+++ Alpine/mirror.sh
@@ -3,10 +3,10 @@ set -euxo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 src="rsync://rsync.alpinelinux.org/alpine/"
 dest="/volume1/mirrors/pub/alpine/"
-mkdir -p $dest
+#mkdir -p $dest
 rsync \
-    --dry-run \
     --verbose \
+    --human-readable \
     --archive \
     --update \
     --compress \
@@ -15,15 +15,10 @@ rsync \
     --delete-after \
     --delay-updates \
     --timeout=600 \
+    --cvs-exclude \
     --no-motd \
     --safe-links --copy-links --hard-links \
     --fuzzy \
-    --exclude="community" \
-    --exclude="main" \
-    --exclude="/MIRRORS.txt" \
-    --exclude="/last-updated" \
-    --exclude="/latest-stable" \
-    --exclude="/edge" \
     --exclude="/v3.0" \
     --exclude="/v3.1" \
     --exclude="/v3.10" \
@@ -36,6 +31,9 @@ rsync \
     --exclude="/v3.7" \
     --exclude="/v3.8" \
     --exclude="/v3.9" \
+    --exclude="*-xen-*" \
+    --exclude="*-standard-*" \
+    --exclude="*_rc*" \
     --exclude-from="$SCRIPT_DIR/../exclude-arch.txt" \
     --include="*/" \
     --include="*.iso" \
@@ -44,13 +42,6 @@ rsync \
     --include="*.iso.sha512" \
     --exclude="*" \
     --cvs-exclude \
+    --prune-empty-dirs \
     $src \
     $dest
-    
-
-# --exclude="*_rc*" \
-# --exclude="*-xen-*" \
-# --exclude="*netboot*" \
-# --exclude="*minirootfs*" \
-# --exclude="*vanilla*" \
-# --exclude="*-standard-*" \
